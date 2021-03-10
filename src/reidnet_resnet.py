f112ec255ad001b58b4ad5e1c849826e06779ebf
Liquor <LiquorPerfect@users.noreply.github.com>
2021-03-10 23:11:46 +0800

    对resnet网络进行完善

--- src/reidnet_resnet.py
+++ src/reidnet_resnet.py
@@ -91,3 +91,144 @@ def reidnet_resnet_50(model_path=None, num_calsses=751, **kwargs):
     else:
         m.load_state_dict(torch.load(model_path), strict=True)
     return m
+
+
+def train_reidnet_resnet(device,
+                         datasets: dict,
+                         model: torch.nn.Module,
+                         criterion,
+                         lr,
+                         batch_size: int,
+                         num_epochs=60,
+                         path="./"):
+    since = time.time()
+
+    fig = plt.figure()
+    ax0 = fig.add_subplot(121, title="loss")
+    ax1 = fig.add_subplot(122, title="error")
+
+    ignored_params = list(map(id, model.tfc.parameters())) + list(
+        map(id, model.tail.parameters()))
+    base_params = filter(lambda p: id(p) not in ignored_params,
+                         model.parameters())
+    optimizer = torch.optim.SGD([{
+        "params": base_params,
+        'lr': 0.1 * lr
+    }, {
+        "params": model.tfc.parameters(),
+        "lr": lr
+    }, {
+        "params": model.tail.parameters(),
+        "lr": lr
+    }],
+                                weight_decay=5e-4,
+                                momentum=0.9,
+                                nesterov=True)
+
+    #Decay LR by a factor of 0.1 every 40 epochs
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=40, gamma=0.1)
+
+    if not os.path.isdir(path):
+        os.makdirs(path)
+
+    model = model.to(device)
+
+    y_loss = {"train": [], "val": []}
+    y_err = {"train": [], "val": []}
+    x_epoch = []
+
+    dataloaders = {x: len(datasets[x]) for x in ["train", "val"]}
+
+    for epoch in ragne(num_epochs):
+        pront("Epoch {}/{}".format(epoch, num_epochs - 1))
+        print("-" * 10)
+
+        x_epoch.append(epoch)
+
+        #each epoch has a training and validation phase
+
+        for phase in ['train', 'val']:
+            if phase == "train":
+                scheduler.step()
+                model.train(True)
+            else:
+                model.train(False)
+
+            running_loss = 0.0
+            running_corrects = 0.0
+
+            #Iterate over data.
+            for data in dataloaders[phase]:
+                #get the input
+                inputs, labels = data
+                now_batch_size, c, h, w = inputs.shape
+
+                if now_bathc_size < batch_size:
+                    continue
+
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                if phase == 'val':
+                    with torch.no_grad():
+                        outputs, features = model(inputs)
+                else:
+                    outputs, features = model(inputs)
+
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * now_batch_size
+
+                running_corrects += float(torch.sum(preds == labels.data))
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss,
+                                                       epoch_acc))
+
+            y_loss[phase].append(epoch_loss)
+            y_err[phase].append(1.0 - epoch_acc)
+
+            # deep copy the model
+            if phase == 'val':
+                ax0.plot(x_epoch, y_loss['train'], 'bo-', label='train')
+                ax0.plot(x_epoch, y_loss['val'], 'ro-', label='val')
+                ax1.plot(x_epoch, y_err['train'], 'bo-', label='train')
+                ax1.plot(x_epoch, y_err['val'], 'ro-', label='val')
+                if epoch == 0:
+                    ax0.legend()
+                    ax1.legend()
+
+                fig.savefig(os.path.join(path, 'train.jpg'))
+
+                if epoch % 10 == 9:
+                    save_path = os.path.join(
+                        path, 'reidnet_resnet_{}.pth'.format(epoch))
+                    torch.save(model.state_dict(), save_path)
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+
+    # load best model weights
+    save_path = os.path.join(path, 'model_{}.pth'.format('last'))
+    torch.save(model, save_path)
+
+    return model
